242868a7d06aaeb6e73c64e873a8eae5a1cb9577
opilane <opilane@HP-CZC6297B82>
2022-12-14 15:38:43 +0200

    Add project files.

---
+++ praktilinetöö_Konjajev_14.12.2022/praktilinetöö_Konjajev_14.12.2022.py
@@ -0,0 +1,32 @@
+﻿from random import *
+from math import *
+
+#2
+#Спросить у пользователя 3 числа, если они окажуться положительными, то проверить могут ли они быть углами одного треугольника(сумма углов 180) и уточнить какого именно треугольника(равносторонний, равнобедренный или разносторонний).
+a=float(input("sisesta arv: "))
+b=float(input("sisesta arv: "))
+c=float(input("sisesta arv: "))
+try:
+    if (a+b+c)>0:
+        print("sobib")
+
+
+
+
+#1Пользователь вводит число, программа определяет знак числа(положительное оно или отрицательное), если число положительное, то проверяет его 
+#на  четность и нечетность.
+try: 
+    a=float(input("sisesta arv: "))
+    if a>0:
+        print("Positiivne")
+        if a%2==0:
+            print(f"{a} on paaris")
+        else:
+            print(f"{a} on paaritud")
+    else:
+            print("Negatiivmne")
+except:
+    print("Numbrid ei sobi")
+  
+
+
